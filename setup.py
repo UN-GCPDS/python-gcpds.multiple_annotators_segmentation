78173a5c666054ef86223f507175df0eedc8037b
Lucas Iturriago <70216214+liturriago@users.noreply.github.com>
2025-02-21 02:08:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multiple_annotators_segmentation',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'tensorflow==2.15.0',
