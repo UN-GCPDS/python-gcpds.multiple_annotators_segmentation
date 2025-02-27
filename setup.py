b04bd0b1d3872cd0a8db4998f8fb0e0c9b5f737e
Lucas Iturriago <liturriago@unal.edu.co>
2025-02-27 12:16:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multiple_annotators_segmentation',
-    version='0.1.0.1',
+    version='0.1.0.2',
     packages=find_packages(),
     install_requires=[
         'tensorflow',
